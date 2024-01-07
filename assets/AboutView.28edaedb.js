87de93b94b9e00472f7dd7420c848dfd1194675c
nyarurato <nyarurato202@gmail.com>
2024-01-08 04:27:24 +0900

    Updates

--- assets/AboutView.28edaedb.js
+++
@@ -1,1 +0,0 @@
-import"./index.7e74fdaf.js";import{_ as r}from"./index.7e74fdaf.js";export{r as default};
---
+++ assets/AboutView.b98fd9f0.js
@@ -0,0 +1,1 @@
+import"./index.3ebc5841.js";import{_ as r}from"./index.3ebc5841.js";export{r as default};
